55482d05f081ba5f095424c9f4991d2db97da711
Aayush701 <ayushghimire7011@gmail.com>
2024-04-21 18:33:56 +0545

    this is good

---
+++ backend/controller/appointmentController.js
@@ -0,0 +1,126 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import ErrorHandler from "../middlewares/error.js";
+import { Appointment } from "../models/appointmentSchema.js";
+import { User } from "../models/userSchema.js";
+
+// creating a appointment for a doctor
+export const postAppointment = catchAsyncErrors(async (req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    nic,
+    dob,
+    gender,
+    appointment_date,
+    department,
+    doctor_firstName,
+    doctor_lastName,
+    hasVisited,
+    address,
+  } = req.body;
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !nic ||
+    !dob ||
+    !gender ||
+    !appointment_date ||
+    !department ||
+    !doctor_firstName ||
+    !doctor_lastName ||
+    !address
+  ) {
+    return next(new ErrorHandler("Please Fill Full Form!", 400));
+  }
+  const isConflict = await User.find({
+    firstName: doctor_firstName,
+    lastName: doctor_lastName,
+    role: "Doctor",
+    doctorDepartment: department,
+  });
+  if (isConflict.length === 0) {
+    return next(new ErrorHandler("Doctor not found", 404));
+  }
+
+  if (isConflict.length > 1) {
+    return next(
+      new ErrorHandler(
+        " many Doctors having same name ! Please Contact Through Email Or Phone!",
+        400,
+      ),
+    );
+  }
+  const doctorId = isConflict[0]._id;
+  const patientId = req.user._id;
+  const appointment = await Appointment.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    nic,
+    dob,
+    gender,
+    appointment_date,
+    department,
+    doctor: {
+      firstName: doctor_firstName,
+      lastName: doctor_lastName,
+    },
+    hasVisited,
+    address,
+    doctorId,
+    patientId,
+  });
+  res.status(200).json({
+    success: true,
+    appointment,
+    message: "Appointment Send!",
+  });
+});
+
+// get all the detail of available appointments
+export const getAllAppointments = catchAsyncErrors(async (req, res, next) => {
+  const appointments = await Appointment.find();
+  res.status(200).json({
+    success: true,
+    appointments,
+  });
+});
+
+// update teh appointment Status
+export const updateAppointmentStatus = catchAsyncErrors(
+  async (req, res, next) => {
+    const { id } = req.params;
+    let appointment = await Appointment.findById(id);
+    if (!appointment) {
+      return next(new ErrorHandler("Appointment not found!", 404));
+    }
+    appointment = await Appointment.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Appointment Status Updated!",
+    });
+  },
+);
+
+// to delete the appointment
+export const deleteAppointment = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const appointment = await Appointment.findById(id);
+  if (!appointment) {
+    return next(new ErrorHandler("Appointment Not Found!", 404));
+  }
+  await appointment.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Appointment Deleted!",
+  });
+});
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+const Hero = ({ title, imageUrl }) => {
+  return (
+    <>
+      <div className="hero container">
+        <div className="banner">
+          <h1>{title}</h1>
+          <p>
+            ZeeCare Medical Institute is a state-of-the-art facility dedicated
+            to providing comprehensive healthcare services with compassion and
+            expertise. Our team of skilled professionals is committed to
+            delivering personalized care tailored to each patient&apos s needs.
+            At ZeeCare, we prioritize your well-being, ensuring a harmonious
+            journey towards optimal health and wellness.
+          </p>
+        </div>
+        <div className="banner">
+          <img src={imageUrl} alt="hero" className="animated-image" />
+          <span>
+            <img src="/Vector.png" alt="vector" />
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+Hero.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+};
+
+export default Hero;
